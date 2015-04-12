cff8e154a76820049cfd6a51dab02c8c2a91826b
Steven Allen <steven@stebalien.com>
2015-04-12 16:11:51 -0400

    Set close on exec.

--- src/imp/unix.rs
+++ src/imp/unix.rs
@@ -18,7 +18,7 @@ fn create_unix(dir: &Path) -> io::Result<File> {
         let name = tmpname();
         let tmp_path = dir.join(OsStr::from_bytes(&name));
         return match unsafe {
-            libc::open(try!(cstr(&tmp_path)).as_ptr(), O_EXCL | O_RDWR | O_CREAT, 0o600)
+            libc::open(try!(cstr(&tmp_path)).as_ptr(), O_CLOEXEC | O_EXCL | O_RDWR | O_CREAT, 0o600)
         } {
             -1 => {
                 let err = io::Error::last_os_error();
@@ -45,7 +45,7 @@ fn create_linux(dir: &Path) -> io::Result<File> {
     let dir = try!(cstr(dir));
 
     match unsafe {
-        libc::open(dir.as_ptr(), O_EXCL | O_TMPFILE | O_RDWR, 0o600)
+        libc::open(dir.as_ptr(), O_CLOEXEC | O_EXCL | O_TMPFILE | O_RDWR, 0o600)
     } {
         -1 => Err(io::Error::last_os_error()),
         fd => Ok(FromRawFd::from_raw_fd(fd)),
