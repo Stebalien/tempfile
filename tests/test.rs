c740a2059a7ad535362f9ad8c922b8a31130fda2
Steven Allen <steven@stebalien.com>
2015-04-13 09:58:05 -0400

    Split tests.

--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,19 @@
 extern crate tempfile;
 use tempfile::TempFile;
-use std::io::{Write, Read};
+use std::io::{Write, Read, Seek, SeekFrom};
 
 #[test]
-fn test() {
+fn test_basic() {
+    let mut tmpfile = TempFile::new_in("/tmp").unwrap();
+    write!(tmpfile, "abcde").unwrap();
+    tmpfile.seek(SeekFrom::Start(0)).unwrap();
+    let mut buf = String::new();
+    tmpfile.read_to_string(&mut buf).unwrap();
+    assert_eq!("abcde", buf);
+}
+
+#[test]
+fn test_share() {
     let mut tmpfile = TempFile::new_in("/tmp").unwrap();
     write!(tmpfile, "abcde").unwrap();
     let mut shared = tmpfile.share().unwrap();
