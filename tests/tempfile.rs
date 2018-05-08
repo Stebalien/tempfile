236183db2f740fda40f0ceff2716c53eacb034d9
Oliver Henshaw <oliver.henshaw@gmail.com>
2018-05-08 12:49:45 +0100

    create_unix should create an unlinked File
    Allows the OS to clean up the file on disk after the last file
    descriptor is closed. This used to be the behaviour of create_unix but
    this was lost after porting to use the util helper in
     bde363d751db4247296ae9345c2e346ec3abe971.
    
    Does not affect linux on systems with O_TMPFILE available.
    
    Add test to assert that temporary files are indeed removed after use.

--- tests/tempfile.rs
+++ tests/tempfile.rs
@@ -1,5 +1,6 @@
 extern crate tempfile;
 use std::io::{Read, Seek, SeekFrom, Write};
+use std::fs;
 
 #[test]
 fn test_basic() {
@@ -9,4 +10,15 @@ fn test_basic() {
     let mut buf = String::new();
     tmpfile.read_to_string(&mut buf).unwrap();
     assert_eq!("abcde", buf);
+}
+
+#[test]
+fn test_cleanup() {
+    let tmpdir = tempfile::tempdir().unwrap();
+    {
+        let mut tmpfile = tempfile::tempfile_in(&tmpdir).unwrap();
+        write!(tmpfile, "abcde").unwrap();
+    }
+    let num_files = fs::read_dir(&tmpdir).unwrap().count();
+    assert!(num_files == 0);
 }
