f21f2151e7d3856e813b70bec8e1b5cf3d32091a
Lzu Tao <taolzu@gmail.com>
2019-07-01 10:06:34 +0700

    build: Bump tempfile itself to 3.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -123,7 +123,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/tempfile/2.2.0"
+    html_root_url = "https://docs.rs/tempfile/3.1.0"
 )]
 #![cfg_attr(test, deny(warnings))]
 
