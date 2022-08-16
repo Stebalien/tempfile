35aabfc64137b7ce21e99530f52461c6c642c9f2
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2022-08-17 00:41:31 +0200

    Replace `fastrand` with `getrandom` and `base64`
    - Instead of setting up a predictable userspace RNG, we get
      unpredictable random bytes directly from the OS. This fixes #178.
    - To obtain a uniformly distributed alphanumeric string, we convert the
      the random bytes to base64 and throw away any letters we don't want
      (`+` and `/`). With a low probability, this may result in obtaining
      too few alphanumeric letters, in which case we request more randomness
      from the OS until we have enough.
    - Because we cannot control the seed anymore, a test manufacturing
      collisions by setting the same seed for several threads had to be removed.

--- src/util.rs
+++ src/util.rs
@@ -1,16 +1,43 @@
 use std::ffi::{OsStr, OsString};
 use std::path::{Path, PathBuf};
-use std::{io, iter::repeat_with};
+use std::io;
 
 use crate::error::IoResultExt;
+
+fn calculate_rand_buf_len(alphanumeric_len: usize) -> usize {
+    let expected_non_alphanumeric_chars = alphanumeric_len / 32;
+    (alphanumeric_len + expected_non_alphanumeric_chars) * 3 / 4 + 3
+}
+
+fn calculate_base64_len(binary_len: usize) -> usize {
+    binary_len * 4 / 3 + 4
+}
+
+fn fill_with_random_base64(rand_buf: &mut [u8], char_buf: &mut Vec<u8>) {
+    getrandom::getrandom(rand_buf).expect("calling getrandom failed");
+    char_buf.resize(calculate_base64_len(rand_buf.len()), 0);
+    base64::encode_config_slice(rand_buf, base64::STANDARD_NO_PAD, char_buf);
+}
 
 fn tmpname(prefix: &OsStr, suffix: &OsStr, rand_len: usize) -> OsString {
     let mut buf = OsString::with_capacity(prefix.len() + suffix.len() + rand_len);
     buf.push(prefix);
-    let mut char_buf = [0u8; 4];
-    for c in repeat_with(fastrand::alphanumeric).take(rand_len) {
-        buf.push(c.encode_utf8(&mut char_buf));
+
+    let mut rand_buf = vec![0; calculate_rand_buf_len(rand_len)];
+    let mut char_buf = vec![0; calculate_base64_len(rand_buf.len())];
+    let mut remaining_chars = rand_len;
+    loop {
+        fill_with_random_base64(&mut rand_buf, &mut char_buf);
+        char_buf.retain(|&c| (c != b'+') & (c != b'/') & (c != 0));
+        if char_buf.len() >= remaining_chars {
+            buf.push(std::str::from_utf8(&char_buf[..remaining_chars]).unwrap());
+            break;
+        } else {
+            buf.push(std::str::from_utf8(&char_buf).unwrap());
+            remaining_chars -= char_buf.len();
+        }
     }
+
     buf.push(suffix);
     buf
 }
