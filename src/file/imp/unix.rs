ecab2b6ae6c093b5b10e748299c44d63e11196a1
Taiki Endo <te316e89@gmail.com>
2023-02-27 03:34:57 +0900

    Update windows-sys to 0.45, redox_syscall to 0.3 (#216)
    * Update windows-sys to 0.45
    
    * Update redox_syscall to 0.3
    
    * Fix unused_variables warning on redox

--- src/file/imp/unix.rs
+++ src/file/imp/unix.rs
@@ -135,7 +135,7 @@ pub fn persist(old_path: &Path, new_path: &Path, overwrite: bool) -> io::Result<
 }
 
 #[cfg(target_os = "redox")]
-pub fn persist(old_path: &Path, new_path: &Path, overwrite: bool) -> io::Result<()> {
+pub fn persist(_old_path: &Path, _new_path: &Path, _overwrite: bool) -> io::Result<()> {
     // XXX implement when possible
     Err(io::Error::from_raw_os_error(syscall::ENOSYS))
 }
