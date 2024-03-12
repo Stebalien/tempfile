617fbe81d438d883a49b549a5d15ab9249c71425
Tobias Stoeckmann <tobias@stoeckmann.org>
2024-03-12 14:18:04 +0100

    chore: set html_root_url to latest

--- src/lib.rs
+++ src/lib.rs
@@ -160,7 +160,7 @@
 #![doc(
     html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
     html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-    html_root_url = "https://docs.rs/tempfile/3.1.0"
+    html_root_url = "https://docs.rs/tempfile/latest/tempfile"
 )]
 #![cfg_attr(test, deny(warnings))]
 #![deny(rust_2018_idioms)]
