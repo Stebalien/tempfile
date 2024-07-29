3e2d1b9b90b58c69286044a030a5222056b2d9cc
Steven Allen <steven@stebalien.com>
2024-07-29 08:52:26 -0700

    doc: improve formatting

--- src/env.rs
+++ src/env.rs
@@ -13,7 +13,7 @@ static DEFAULT_TEMPDIR: OnceLock<PathBuf> = OnceLock::new();
 /// Only the first call to this function will succeed. All further calls will fail with `Err(path)`
 /// where `path` is previously set default temporary directory override.
 ///
-/// NOTE: This function does not check if the specified directory exists and/or is writable.
+/// **NOTE:** This function does not check if the specified directory exists and/or is writable.
 pub fn override_temp_dir(path: &Path) -> Result<(), PathBuf> {
     let mut we_set = false;
     let val = DEFAULT_TEMPDIR.get_or_init(|| {
@@ -33,7 +33,7 @@ pub fn override_temp_dir(path: &Path) -> Result<(), PathBuf> {
 /// This function simply delegates to [`std::env::temp_dir`] unless the default temporary directory
 /// has been override by a call to [`override_temp_dir`].
 ///
-/// NOTE: This function does check if the returned directory exists and/or is writable.
+/// **NOTE:** This function does check if the returned directory exists and/or is writable.
 pub fn temp_dir() -> PathBuf {
     DEFAULT_TEMPDIR
         .get()
