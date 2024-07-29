a1b4bb56c47725165cc66f13b2c99c5e38d94895
Steven Allen <steven@stebalien.com>
2024-07-29 08:44:45 -0700

    fix: address feedback

--- src/env.rs
+++ src/env.rs
@@ -6,23 +6,24 @@ use once_cell::sync::OnceCell as OnceLock;
 
 static DEFAULT_TEMPDIR: OnceLock<PathBuf> = OnceLock::new();
 
-/// Override the default temporary directory (defaults to [`std::env::temp_dir`]. This function
+/// Override the default temporary directory (defaults to [`std::env::temp_dir`]). This function
 /// changes the _global_ default temporary directory for the entire program and should not be called
 /// except in exceptional cases where it's not configured correctly by the platform.
 ///
-/// Only the first call to this function will succeed, returning `Ok(path)`. All further calls will
-/// fail with `Err(path)`. In both cases, `path` is the default temporary directory (on success, the
-/// new one; on failure, the existing one).
-pub fn override_temp_dir(path: &Path) -> Result<&'static Path, &'static Path> {
+/// Only the first call to this function will succeed. All further calls will fail with `Err(path)`
+/// where `path` is previously set default temporary directory override.
+///
+/// NOTE: This function does not check if the specified directory exists and/or is writable.
+pub fn override_temp_dir(path: &Path) -> Result<(), PathBuf> {
     let mut we_set = false;
     let val = DEFAULT_TEMPDIR.get_or_init(|| {
         we_set = true;
         path.to_path_buf()
     });
     if we_set {
-        Ok(val)
+        Ok(())
     } else {
-        Err(val)
+        Err(val.to_owned())
     }
 }
 
@@ -31,6 +32,8 @@ pub fn override_temp_dir(path: &Path) -> Result<&'static Path, &'static Path> {
 ///
 /// This function simply delegates to [`std::env::temp_dir`] unless the default temporary directory
 /// has been override by a call to [`override_temp_dir`].
+///
+/// NOTE: This function does check if the returned directory exists and/or is writable.
 pub fn temp_dir() -> PathBuf {
     DEFAULT_TEMPDIR
         .get()
