5151f744d3e64a8add31aa56886b2a9a8638e34d
Steven Allen <steven@stebalien.com>
2024-11-10 11:00:35 +0700

    fix: forbid path separators in prefix/suffix

--- src/lib.rs
+++ src/lib.rs
@@ -316,7 +316,7 @@ impl<'a> Builder<'a> {
 
     /// Set a custom filename prefix.
     ///
-    /// Path separators are legal but not advisable.
+    /// Path separators are not allowed.
     /// Default: `tmp`.
     ///
     /// # Examples
@@ -336,7 +336,7 @@ impl<'a> Builder<'a> {
 
     /// Set a custom filename suffix.
     ///
-    /// Path separators are legal but not advisable.
+    /// Path separators are not allowed.
     /// Default: empty.
     ///
     /// # Examples
