af653d01db18eab54e3bff567d6b849cc9d2f1df
Steven Allen <steven@stebalien.com>
2025-08-19 18:09:07 -0700

    mount as volume and directly run with adb shell

--- .github/workflows/run-on-android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CMD="$1"
-FNAME="/data/local/tmp/$(basename "$CMD")"
-
-shift
-
-adb -s localhost:5555 push "$CMD" "$FNAME"
-adb -s localhost:5555 shell "cd /data/local/tmp && chmod 755 $FNAME && $FNAME $*"
