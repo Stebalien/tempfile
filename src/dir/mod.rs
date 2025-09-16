870b4799979926c52a384b63f8e6aaa0a5d09921
Redddy <midzy0228@gmail.com>
2025-09-16 15:30:15 +0900

    doc: Update COPYRIGHT link

--- src/dir/mod.rs
+++ src/dir/mod.rs
@@ -1,6 +1,6 @@
 // Copyright 2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
+// https://github.com/rust-lang/rust/blob/master/COPYRIGHT.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
